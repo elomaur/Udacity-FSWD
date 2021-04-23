6e310d9249eab7a39a1fc7e7bd7c1928b41a2822
Elom <babyshyne307@hotmail.com>
2021-04-22 19:46:32 -0600

    d3

--- class-demos/todoapp/app.py
+++ class-demos/todoapp/app.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request, redirect, url_for, jsonify
 from flask_sqlalchemy import SQLAlchemy
+import sys
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://Maurice@localhost:5432/todoapp'
@@ -17,6 +17,24 @@ class Todo(db.Model):
 
 db.create_all()
 
+@app.route('/todos/create', methods=['POST'])
+def create_todo():
+    error = False
+    body ={}
+    try:
+        description = request.get_json()['description']
+        todo = Todo(description=description)
+        db.session.add(todo)
+        db.session.commit()
+        body['description'] = todo.description
+    except:
+        error = True
+        db.session.rollback()
+        print(sys.exc_info())
+    finally:
+        db.session.close()
+    if not error:
+        return jsonify(body)
 
 @app.route('/')
 def index():
